756739da24e15e546b2392e199e5ea982f0102eb
Dalton Pinto <efqdalton@gmail.com>
2010-10-18 17:17:19 -0200

    Using Jeweler for releasing

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'init.rb'
+require 'rake'
 
 task :create_database do
   if ENV['database']
@@ -22,4 +24,19 @@ task :create_views do
       database.create_views
     end
   end
+end
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "divan"
+    gem.summary = "A Ruby CouchDB Client for insane people"
+    gem.description = "This is a very simple CouchDB client that have few dependencies.\nThis client has a lot of interesting features, for example: easy access to CouchDB revisions.\n"
+    gem.email = "dalthon@aluno.ita.br"
+    gem.homepage = "http://github.com/efqdalton/divan"
+    gem.authors = ["Dalton Pinto"]
+  end
+  Jeweler::GemcutterTasks.new
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
 end
